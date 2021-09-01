5efc6470baf83825b01b5887e32cbedc6bc22d64
Leonardo Geja <bemmenos@gmail.com>
2021-09-01 17:25:14 -0300

    chore: add tpyeorm

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express from 'express';
+import routes from './routes/homeRoutes';
+
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(routes);
+
+// Servidor
 app.listen(8000, () => {
 	console.log('rodando');
 });
