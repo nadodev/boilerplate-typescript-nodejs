6f36a24cfa87e6918d95c69d19bba5b7abaf56a5
Leonardo Geja <bemmenos@gmail.com>
2021-09-01 17:49:01 -0300

    chore: add page error

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import express from 'express';
-import routes from './routes/homeRoutes';
+import express, { NextFunction, Request, Response } from 'express';
+import AppError from './shared/errors/AppError';
+import routes from './shared/routes/homeRoutes';
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(routes);
-
+app.use(
+	(error: Error, request: Request, response: Response, next: NextFunction) => {
+		if (error instanceof AppError) {
+			return response.status(error.statusCode).json({
+				status: 'error',
+				message: error.message,
+			});
+		}
+		return response.status(500).json({
+			status: 'error',
+			message: 'Internal Server Error',
+		});
+	},
+);
 // Servidor
 app.listen(8000, () => {
 	console.log('rodando');
